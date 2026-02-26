6e858472f5c2ddd707e4afab676b0015a085e9f2
sahilsaharan749-cell <sahilsaharan749@gmail.com>
2026-02-26 13:47:40 +0530

    Create problem27.py

---
+++ homework6/problem27.py
@@ -0,0 +1,47 @@
+def spiral_traversal(mat):
+    result = []
+    
+    if not mat:
+        return result
+    
+    top = 0
+    bottom = len(mat) - 1
+    left = 0
+    right = len(mat[0]) - 1
+    
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            result.append(mat[top][i])
+        top += 1
+        
+        for i in range(top, bottom + 1):
+            result.append(mat[i][right])
+        right -= 1
+        
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(mat[bottom][i])
+            bottom -= 1
+        
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(mat[i][left])
+            left += 1
+    
+    return result
+
+
+mat1 = [[1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]]
+print(spiral_traversal(mat1))
+
+mat2 = [[1, 2, 3, 4, 5, 6],
+        [7, 8, 9, 10, 11, 12],
+        [13, 14, 15, 16, 17, 18]]
+print(spiral_traversal(mat2))
+
+mat3 = [[32, 44, 27, 23],
+        [54, 28, 50, 62]]
+print(spiral_traversal(mat3))
