47151d4c130fa02842d4ac402df6da46d9757b3f
sahilsaharan749-cell <sahilsaharan749@gmail.com>
2026-02-26 13:31:30 +0530

    Create problem12.py

---
+++ homework3/problem12.py
@@ -0,0 +1,28 @@
+def get_min_diff(arr, k):
+    n = len(arr)
+    arr.sort()
+
+    ans = arr[-1] - arr[0]
+    small = arr[0] + k
+    big = arr[-1] - k
+
+    if small > big:
+        small, big = big, small
+
+    for i in range(1, n - 1):
+        subtract = arr[i] - k
+        add = arr[i] + k
+
+        if subtract >= small or add <= big:
+            continue
+
+        if big - subtract <= add - small:
+            small = subtract
+        else:
+            big = add
+
+    return min(ans, big - small)
+
+
+print(get_min_diff([1, 5, 8, 10], 2))
+print(get_min_diff([3, 9, 12, 16, 20], 3))
